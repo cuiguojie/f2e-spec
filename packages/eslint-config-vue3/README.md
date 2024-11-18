45423bfabee68467df24bb63a0cb0a9c2090a833
cuiguojie <cuiguojie@me.com>
2024-11-18 14:42:51 +0800

    feat(eslint): vue3 项目 eslint 升级到 9 的配置格式

--- packages/eslint-config-vue3/README.md
+++ packages/eslint-config-vue3/README.md
@@ -2,10 +2,15 @@
 
 源生 vue3 + typescript 项目的 通用 eslint-config 配置。
 
+## 版本说明
+`1.0.0` 之前的版本，基于 `eslint@8` 的规则。
+
+`1.0.0` 及后续版本，基于 `eslint@9` 的 flat 格式规则。
+
 ## 功能说明
 
-* 集成了官方脚手架自带的 ESLint 配置项，及依赖包
-* 修改官方校验规则等级（`essential` 改为 `recommended`）
+* 集成了 Vue 官方脚手架自带的 ESLint 配置项，及依赖包
+* 修改 Vue 官方校验规则等级（`essential` 改为 `recommended`）
 * 添加团队常用的自定义规则覆写
 
 ## 使用：
@@ -15,6 +20,7 @@
 pnpm i @cuiguojie/eslint-config-vue3 -D
 ```
 
+### 1.0.0 之前版本（eslint 8）
 配置 `.eslintrc.cjs` （或者其他格式的 eslintrc 文件）：
 
 ```
@@ -27,11 +33,25 @@ module.exports = {
 ```
 
 > 如果是从 `create-vue` 创建的项目，需要清理已经封装过的脚手架自带的依赖
+
 ```
 pnpm uninstall @rushstack/eslint-patch @vue/eslint-config-typescript eslint-plugin-vue
 ```
 
 > 如果项目 packages.json 中已经有低版本的 eslint，需要按照 peer 提示的说明升级或者重新安装
+
+### 1.0.0 及后续版本（eslint 9）
+配置 `.eslint.config.js` （或者其他格式的 eslint 配置 文件）：
+
+> 注意：ESLint 9 放弃了所有 `eslintrc` 格式的配置，统一使用 `config` 格式，如 `eslint.config.js` 或 `eslint.config.mjs`
+
+```
+import config from '@cuiguojie/eslint-config-vue3';
+
+export default [
+  ...config
+];
+```
 
 ## 其他
 eslint 的多个 extends 配置存在 **覆盖及合并** 操作，如果需要查看最终结果，在调用方项目中，执行以下命令可以把最终结果保存到指定的文件查看：
--- packages/eslint-config-vue3/index.js
+++ packages/eslint-config-vue3/index.js
@@ -1,29 +1,46 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+import pluginVue from 'eslint-plugin-vue';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
 
-module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    '@vue/eslint-config-typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
   },
-  rules: {
-    // Basic
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'comma-dangle': ['error', 'never'],
-    // Vue
-    'vue/no-v-html': 'off',
-    'vue/multi-word-component-names': ['error', {
-      'ignores': [
-        'Home',
-        'Overview'
-      ]
-    }]
-    // add your custom rules here
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
+
+  ...pluginVue.configs['flat/recommended'],
+  ...vueTsEslintConfig(),
+
+  {
+    name: 'app/custom-rules',
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'never']
+    }
+  },
+  {
+    name: 'app/custom-vue-rules',
+    rules: {
+      'vue/no-v-html': 'off',
+      'vue/max-attributes-per-line': [
+        'warn',
+        {
+          singleline: 1
+        }
+      ],
+      'vue/multi-word-component-names': ['error', {
+        'ignores': [
+          'index',
+          'Home',
+          'List',
+          'Overview'
+        ]
+      }]
+    }
   }
-};
+];
