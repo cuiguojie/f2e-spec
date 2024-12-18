f5dc9034266131909a90733f7e7568d613a676a6
cuiguojie <cuiguojie@kanjian.com>
2024-12-18 17:23:58 +0800

    feat(eslint-config-vue3): 升级 eslint 版本

--- packages/eslint-config-vue3/index.js
+++ packages/eslint-config-vue3/index.js
@@ -9,7 +9,12 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      '**/.*/**'
+    ]
   },
 
   ...pluginVue.configs['flat/recommended'],
